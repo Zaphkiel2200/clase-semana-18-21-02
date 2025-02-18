fc9a4d092e049aa20098bd62c747da3f22d750ce
Zaphkiel2200 <135277854+Zaphkiel2200@users.noreply.github.com>
2025-02-18 10:22:30 -0500

    add

---
+++ index.js
@@ -0,0 +1,7 @@
+// Código to deep copy en un objeto  (object) ando medio pérdido xd
+
+let original = { a: 1, b: { c: 2 } };
+let deepCopy = JSON.parse(JSON.stringify(original));
+
+deepCopy.b.c = 3;
+console.log(original.b.c); 
